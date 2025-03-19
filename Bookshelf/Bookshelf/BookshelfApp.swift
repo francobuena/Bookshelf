4d91f88590ffbdaafbb46b71e1151532a06c9789
francobuena <francobuena@outlook.com>
2025-03-19 21:50:50 +1100

    initial commit

---
+++ Bookshelf/Bookshelf/BookshelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookshelfApp.swift
+//  Bookshelf
+//
+//  Created by Buena, Franco on 19/3/2025.
+//
+
+import SwiftUI
+
+@main
+struct BookshelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bookshelf/Bookshelf/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bookshelf
+//
+//  Created by Buena, Franco on 19/3/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
